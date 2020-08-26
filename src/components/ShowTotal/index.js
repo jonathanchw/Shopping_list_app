4c15881d213557892fca52d0c4f5d0fb76df3f77
Jonathan Chacon <Jonathanchw@gmail.com>
2020-08-26 17:22:30 -0300

    adding features

--- src/components/ShowTotal/index.js
+++ src/components/ShowTotal/index.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import './style.css';
 import thumbUp from '../../assets/thumbUp.png';
+import thumbDown from '../../assets/thumbDown.png';
 
 
-const ShowTotal=()=> {
- 
+const ShowTotal = ({ total, showMessage }) => {
+
   return (
     <div className="ShowTotal_container">
       <div className="ShowTotal_Money">
-      <span>260$</span>
-      <span>Total</span>
+        <span>{total}</span>
+        <span>Total</span>
       </div>
-      <div className="ShowTotal_message">
-        <p>Aun te queda dinero</p>
-        <img className="thumbUp" src={thumbUp} alt="Thumb Up" />
-      </div>
+
+      {showMessage === true ?
+        <div className="ShowTotal_message">
+          <p>Aun te queda dinero</p>
+          <img className="thumbUp" src={thumbUp} alt="Thumb Up" />
+        </div> :
+        <div className="ShowTotal_message">
+          <p>Ya NO te queda dinero</p>
+          <img className="thumbUp" src={thumbDown} alt="Thumb Down" />
+        </div>
+      }
+
+
     </div>
   )
 }
--- src/components/useListState/index.js
+++ src/components/useListState/index.js
@@ -1,43 +1,54 @@
 import { useState } from 'react'
 
 export default (initialValue) => {
+
   const [list, setList] = useState(initialValue);
 
-  // --resolver aca la suma de los precios y parsarlos al componente ShowTotal--//
+
+  //--total devuelve la suma de todos los precios de los items----//
   const arrPrecio = [0];
   list.map((item) => {
     return item.precio !== 0 ? arrPrecio.push(parseFloat(item.precio, 10)) : null
   });
-  const sumaTotal = arrPrecio.reduce((acumulator, current) => acumulator + current);
-  //--sumaTotal devuelve la suma de todos los precios de los items----//
-console.log(sumaTotal);
+  const total = arrPrecio.reduce((acumulator, current) => acumulator + current);
+  const showMessage = false;
+
   return {
     list,
+    total,
+    showMessage,
     addItem: (itemText) => {
       const trimmedText = itemText.descripcion.trim();
       if (trimmedText.length > 0) {
         setList([...list, itemText])
       };
     },
+
     deleteItem: (itemIndex) => {
       const newList = list.filter((_, index) => index !== itemIndex);
       setList(newList);
     },
+
     checkItem: (itemIndex) => {
       itemIndex.check = true;
       setList([...list])
-
     },
+
     addPrecio: (itemPrecio) => {
       (list[itemPrecio.getIndex].precio = itemPrecio.value)
       setList([...list]);
     },
+
     goBack: (itemIndex) => {
       itemIndex.check = false;
       setList([...list])
+    },
 
-    },sumaTotal
-    
-
+    addCash: (showcash) => {
+      /*por resolver aun logica para que showmessage cambie.
+      const cash = parseFloat(showcash.value, 10);
+           console.log(total)
+*/
+    }
   }
 }
