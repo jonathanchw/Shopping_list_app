2031978496e844e717ff026c45b46aaa0cdced0f
Jonathan Chacon <Jonathanchw@gmail.com>
2020-09-01 21:59:14 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -4,15 +4,11 @@ import './App.css';
 import Header from './components/Header/';
 import List from './components/List';
 
-
-
 function App() {
   return (
     <div className="App">
       <Header />
-
       <List />
-      
     </div>
   );
 }
