e4bda7e40376851f157c41076b8a2be331da3338
Jonathan Chacon <Jonathanchw@gmail.com>
2020-09-06 20:10:41 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, lazy, Suspense } from 'react';
 import './App.css';
-import List from './components/List';
+const renderLoader = () => <p>Loading</p>;
+const List = lazy(() => import('./components/List'));
+
 
 function App() {
-  let intitialListasArr=[]
+  let intitialListasArr = []
   if (localStorage.getItem("myListas") !== null) {
     let dataListas = localStorage.getItem("myListas");
     intitialListasArr = JSON.parse(dataListas);
-     }
+  }
   const [listas, setListas] = useState(intitialListasArr);
   localStorage.setItem("myListas", JSON.stringify(listas));
   return (
-    <div className="App">
-      <List setListas={setListas} listas={listas} />
-    </div>
+    <Suspense fallback={renderLoader()}>
+      <div className="App">
+        <List setListas={setListas} listas={listas} />
+      </div>
+    </Suspense>
   );
 }
 
